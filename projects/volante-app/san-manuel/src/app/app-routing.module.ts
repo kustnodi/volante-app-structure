9a0212c1862acff4ccd54c57c48850ff53ad3ec1
Dustin Hodges <dustin@volantesoftware.com>
2022-04-18 13:32:55 -0600

    lazy load main machines routes

--- projects/volante-app/san-manuel/src/app/app-routing.module.ts
+++ projects/volante-app/san-manuel/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'machines',
+    loadChildren: () => import('@volante/slottrak-machines').then(m => m.SlottrakMachinesRoutedModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
