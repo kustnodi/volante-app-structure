9e276e65ad6d02d4dbdef35a771f127acfe07eda
Dustin Hodges <dustin@volantesoftware.com>
2022-04-20 15:57:44 -0600

    adding main machine components

---
+++ projects/volante/slottrak-machines/src/lib/slottrak-machines-main/components/detail/configurations/configurations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'slottrak-machines-configurations',
+  templateUrl: './configurations.component.html',
+  styleUrls: ['./configurations.component.css']
+})
+export class ConfigurationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
