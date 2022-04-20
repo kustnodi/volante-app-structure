f8527450a54780349572912eee8cdd64b50df5fe
Dustin Hodges <dustin@volantesoftware.com>
2022-04-20 11:39:49 -0600

    get rid of hard coded routes

--- projects/volante-app/san-manuel/src/app/app-routing.module.ts
+++ projects/volante-app/san-manuel/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'machines',
-    loadChildren: () => import('@volante/slottrak-machines').then(m => m.SlottrakMachinesRoutedModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
