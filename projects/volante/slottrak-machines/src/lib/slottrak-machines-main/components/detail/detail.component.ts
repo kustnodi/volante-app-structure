fd06edbef507a44c324e9ab07faa5dcc7fb54231
Dustin Hodges <dustin@volantesoftware.com>
2022-04-21 11:26:55 -0600

    inject configured machine detail routes

--- projects/volante/slottrak-machines/src/lib/slottrak-machines-main/components/detail/detail.component.ts
+++ projects/volante/slottrak-machines/src/lib/slottrak-machines-main/components/detail/detail.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Inject, Injector, OnInit } from '@angular/core';
+import { DetailRouteConfig } from '@volante/slottrak-app';
+import { MACHINE_DETAIL_ROUTES } from '@volante/slottrak-machines/src/lib/slottrak-machines-services';
 @Component({
   selector: 'slottrak-machines-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(@Inject(MACHINE_DETAIL_ROUTES) readonly detailRoutes: DetailRouteConfig[], injector: Injector) {
   }
 
+  ngOnInit(): void {
+
+  }
 }
