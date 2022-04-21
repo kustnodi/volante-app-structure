b07e86f69a6c581ded54479ee1599d588b2fc099
Dustin Hodges <dustin@volantesoftware.com>
2022-04-21 11:27:30 -0600

    export services symbols

--- projects/volante/slottrak-machines/src/public-api.ts
+++ projects/volante/slottrak-machines/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of slottrak-machines
  */
 
+export * from '@volante/slottrak-machines/src/lib/slottrak-machines-services';
 export * from '@volante/slottrak-machines/src/lib/slottrak-machines-config';
 export * from '@volante/slottrak-machines/src/lib/slottrak-machines-main';
