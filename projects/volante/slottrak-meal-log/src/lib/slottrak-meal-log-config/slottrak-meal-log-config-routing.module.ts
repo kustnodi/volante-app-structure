fcfd0162d178ea358c675e05a1ddc41ec0c9ee0a
Dustin Hodges <dustin@volantesoftware.com>
2022-04-22 10:39:29 -0600

    moved machine detail state provider to routing   module

--- projects/volante/slottrak-meal-log/src/lib/slottrak-meal-log-config/slottrak-meal-log-config-routing.module.ts
+++ projects/volante/slottrak-meal-log/src/lib/slottrak-meal-log-config/slottrak-meal-log-config-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { MACHINE_DETAIL_ROUTES } from '@volante/slottrak-machines';
 
 const routes: Routes = [
   {
@@ -15,6 +15,19 @@ const routes: Routes = [
   ],
   exports: [
     RouterModule
+  ],
+  providers: [
+    {
+      provide: MACHINE_DETAIL_ROUTES,
+      multi: true,
+      useValue: {
+        displayText: 'MEAL Log',
+        route: {
+          path: 'meal-log',
+          loadChildren: () => import('@volante/slottrak-meal-log/src/lib/slottrak-meal-log-machine-detail').then(m => m.SlottrakMealLogMachineDetailModule)
+        }
+      }
+    }
   ]
 })
 export class SlottrakMealLogConfigRoutingModule { }
--- projects/volante/slottrak-meal-log/src/lib/slottrak-meal-log-config/slottrak-meal-log-config.module.ts
+++ projects/volante/slottrak-meal-log/src/lib/slottrak-meal-log-config/slottrak-meal-log-config.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { MACHINE_DETAIL_ROUTES } from '@volante/slottrak-machines';
 import { SlottrakMealLogConfigRoutingModule } from './slottrak-meal-log-config-routing.module';
 
 @NgModule({
@@ -8,17 +7,6 @@ import { SlottrakMealLogConfigRoutingModule } from './slottrak-meal-log-config-r
     SlottrakMealLogConfigRoutingModule
   ],
   providers: [
-    {
-      provide: MACHINE_DETAIL_ROUTES,
-      multi: true,
-      useValue: {
-        displayText: 'MEAL Log',
-        route: {
-          path: 'meal-log',
-          loadChildren: () => import('@volante/slottrak-meal-log/src/lib/slottrak-meal-log-machine-detail').then(m => m.SlottrakMealLogMachineDetailModule)
-        }
-      }
-    }
   ]
 })
 export class SlottrakMealLogConfigModule { }
