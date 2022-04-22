a22632055990cf01f6a6db354097931432e03e30
Dustin Hodges <dustin@volantesoftware.com>
2022-04-22 15:26:16 -0600

    use common slottrak app component

--- projects/volante-app/san-manuel/src/app/app.module.ts
+++ projects/volante-app/san-manuel/src/app/app.module.ts
@@ -1,28 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SlottrakMachinesConfigModule } from '@volante/slottrak-machines';
 import { RouterModule } from '@angular/router';
 import { SlottrakMealLogConfigModule } from '@volante/slottrak-meal-log';
-import { SlottrakAppUiModule } from '@volante/slottrak-app';
+import { SlottrakAppModule } from '@volante/slottrak-app';
 import { SlottrakProgressivesConfigModule } from '@volante/slottrak-progressives';
+import { SlotTrakAppComponent } from 'projects/volante/slottrak-app/src/lib/slottrak-app';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule,
     RouterModule.forRoot([]),
-    BrowserAnimationsModule,
+    SlottrakAppModule,
     SlottrakMachinesConfigModule,
     SlottrakMealLogConfigModule,
-    SlottrakProgressivesConfigModule,
-    SlottrakAppUiModule
+    SlottrakProgressivesConfigModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [SlotTrakAppComponent]
 })
 export class AppModule {
 }
