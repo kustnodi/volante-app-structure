2f3da599c995d6c4f50b460248647562e7966bbe
Dustin Hodges <dustin@volantesoftware.com>
2022-04-22 13:14:20 -0600

    provide machine detail lazy load route

--- projects/volante/slottrak-progressives/src/lib/slottrak-progressives-config/slottrak-progressives-config-routing.module.ts
+++ projects/volante/slottrak-progressives/src/lib/slottrak-progressives-config/slottrak-progressives-config-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MACHINE_DETAIL_ROUTES } from '@volante/slottrak-machines';
 
 const routes: Routes = [
   {
@@ -10,6 +11,19 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: MACHINE_DETAIL_ROUTES,
+      multi: true,
+      useValue: {
+        displayText: 'Progressives',
+        route: {
+          path: 'progressives',
+          loadChildren: () => import('@volante/slottrak-progressives/src/lib/slottrak-progressives-machine-detail').then(m => m.SlottrakProgressivesMachineDetailModule)
+        }
+      }
+    }
+  ]
 })
 export class SlottrakProgressivesConfigRoutingModule { }
