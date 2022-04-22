6d34e26dd2b9e6ca2ac9da8203169d045159a8d1
Dustin Hodges <dustin@volantesoftware.com>
2022-04-22 13:05:10 -0600

    added progressives machine detail module

---
+++ projects/volante/slottrak-progressives/src/lib/slottrak-progressives-machine-detail/slottrak-progressives-machine-detail.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SlottrakProgressivesMachineDetailRoutingModule } from './slottrak-progressives-machine-detail-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SlottrakProgressivesMachineDetailRoutingModule
+  ]
+})
+export class SlottrakProgressivesMachineDetailModule { }
