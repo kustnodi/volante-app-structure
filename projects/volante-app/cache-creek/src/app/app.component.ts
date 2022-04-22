7e56eb7ce5820c8ee208bf6075cb5d434178709f
Dustin Hodges <dustin@volantesoftware.com>
2022-04-22 13:46:28 -0600

    fixing prefix in cache creek

--- projects/volante-app/cache-creek/src/app/app.component.ts
+++ projects/volante-app/cache-creek/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'slottrak-san-manuel-root',
+  selector: 'slottrak-cache-creek-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
