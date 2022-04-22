b75f59b94a5bb58df3e88b8f08dc1742dd7b6f68
Dustin Hodges <dustin@volantesoftware.com>
2022-04-22 13:06:34 -0600

    added entry component

--- projects/volante/slottrak-progressives/src/lib/slottrak-progressives-machine-detail/slottrak-progressives-machine-detail.module.ts
+++ projects/volante/slottrak-progressives/src/lib/slottrak-progressives-machine-detail/slottrak-progressives-machine-detail.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SlottrakProgressivesMachineDetailRoutingModule } from './slottrak-progressives-machine-detail-routing.module';
+import { EntryComponent } from './components/entry/entry.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EntryComponent
+  ],
   imports: [
     CommonModule,
     SlottrakProgressivesMachineDetailRoutingModule
