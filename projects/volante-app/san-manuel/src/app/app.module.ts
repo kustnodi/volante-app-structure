67e0878de259c0664bbd7f1f600d6e59791388c3
Dustin Hodges <dustin@volantesoftware.com>
2022-04-22 15:27:26 -0600

    import from correct location

--- projects/volante-app/san-manuel/src/app/app.module.ts
+++ projects/volante-app/san-manuel/src/app/app.module.ts
@@ -4,9 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SlottrakMachinesConfigModule } from '@volante/slottrak-machines';
 import { RouterModule } from '@angular/router';
 import { SlottrakMealLogConfigModule } from '@volante/slottrak-meal-log';
-import { SlottrakAppModule } from '@volante/slottrak-app';
+import { SlottrakAppModule, SlotTrakAppComponent } from '@volante/slottrak-app';
 import { SlottrakProgressivesConfigModule } from '@volante/slottrak-progressives';
-import { SlotTrakAppComponent } from 'projects/volante/slottrak-app/src/lib/slottrak-app';
 
 @NgModule({
   imports: [
